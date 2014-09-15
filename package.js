189f15ea3a6b0cb7eb60383df660376846bad379
parhelium <me@parhelium.pl>
2014-09-15 21:26:14 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary: "EpicEditor",
+  version: "0.2.2",
+  git: " ",
+  name: "parhelium:epic-editor"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+  var where = ['client'];
+  var path = Npm.require('path');
+
+  api.add_files(path.join('v0.2.2','js','epiceditor.js'), where);
+  api.add_files(path.join('v0.2.2','themes','base','epiceditor.css'), where);
+  api.add_files(path.join('v0.2.2','themes','editor','epic-light.css'), where);
+  api.add_files(path.join('v0.2.2','themes','preview','github.css'), where);
+
+  api.export('EpicEditor', 'client');
+
+});
