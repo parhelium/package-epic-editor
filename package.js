fe05b1435904124fb029134a034339079d1cddbc
parhelium <me@parhelium.pl>
2014-09-15 23:09:58 +0200

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "EpicEditor",
-  version: "0.2.3",
+  summary: "Markdown editor - EpicEditor",
+  version: "0.2.4",
   git: "https://github.com/parhelium/package-epic-editor",
   name: "parhelium:epic-editor"
 });
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   var where = ['client'];
   var path = Npm.require('path');
-  api.use("parhelium:marked");
+  api.use("parhelium:marked@0.3.3");
 
   api.add_files(path.join('v0.2.2','js','epiceditor.js'), where);
   api.add_files(path.join('v0.2.2','themes','base','epiceditor.css'), where);
